2310186099ea69cec0696a0d9ed1cb466125a31e
mariah622 <mjustine622@gmail.com>
2022-01-10 22:59:15 -0500

    commit tree ad2fe0e168eb60d244c0d5c8f891eb4a2f9cf8d3

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 class Formatter {
+
+  static capitalize(string) {
+    return string.charAt().toUpperCase() + string.slice(1)
+
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+
+  }
+
+  static titleize(string) {
+    let exceptions = [ 'the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from' ]
+    let final = [];
+    let arrayOfWords = string.split(" ")
+    for ( let i = 0; i < arrayOfWords.length; i++) {
+      if ( i == 0 ) {
+        final.push(this.capitalize( arrayOfWords[i]))
+      } else {
+        if (exceptions.includes(arrayOfWords[i])) {
+          final.push(arrayOfWords[i])
+        } else {
+          final.push(this.capitalize(arrayOfWords[i]))
+        }
+      }
+
+    }
+    return final.join( " " );
+  }
   //add static methods here
 }
